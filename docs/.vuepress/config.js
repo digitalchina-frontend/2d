b7b48a1b74dc199693f8fe1aef6a3373d9ad9d15
fancn21th <fancn21th@gmail.com>
2022-06-05 14:57:19 +0800

    基本目录结构

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,16 @@ module.exports = {
   title: "2d",
   description: "2d",
   serviceWorker: true,
-  themeConfig: {},
+  themeConfig: {
+    sidebar: "auto",
+    sidebarDepth: 2,
+    nav: [
+      { text: "SVG", link: "/svg/" },
+      { text: "Canvas", link: "/canvas/" },
+      { text: "WebGL", link: "/webgl/" },
+      { text: "H5", link: "/h5/" },
+    ],
+    lastUpdated: "Last Updated", // string | boolean
+  },
   plugins: [],
 };
